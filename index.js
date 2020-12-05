1459776a99ebaff2c15a0d0cd16917027b2040d0
kokhenglim <kokhenglim>
2020-12-05 14:52:06 +0800

    add remove task button add feature, refactor code

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const addTaskBtn = document.getElementById("btnAddTask");
-let taskInput = document.getElementById("input-task");
-
-addTaskBtn.addEventListener("click", function () {
-  if (taskInput.value == "" || taskInput.value == undefined) {
-   return  alert("please input tasks");
+document.addEventListener("click", function (event) {
+  if (event.target.id == "btnAddTask") {
+    addTask();
   }
 
-  let html = "<li>" + taskInput.value + "</li>";
+  if (event.target.classList.contains("btn-remove-task")) {
+    event.target.parentNode.remove();
+  }
+});
+
+function addTask() {
+  let taskInput = document.getElementById("input-task");
+
+  if (taskInput.value == "" || taskInput.value == undefined) {
+    return alert("please input tasks");
+  }
+
+  let html =
+    "<li>" +
+    taskInput.value +
+    ' <button class="btn btn-primary btn-complete-task">Complete Task</button> <button class="btn btn-danger btn-remove-task">Remove Task</button></li>';
 
   let todoList = document.getElementById("todo-list");
   todoList.innerHTML += html;
 
   // clear the task input
-  taskInput.value = '';
-});
+  taskInput.value = "";
+  taskInput.focus();
+}
